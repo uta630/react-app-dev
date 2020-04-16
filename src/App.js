fe3f0e630955cfd46262e6693d50fb588976eb90
臼井健志 <ta-usui@media-plex.net>
2020-04-16 13:32:22 +0900

    jsx構文のルール（発展）

--- src/App.js
+++ src/App.js
@@ -6,8 +6,26 @@ function App() {
   // 文字列の変数化した場合、{変数名}でjsxに挿入できる
   const text = "Hello, world!!!";
   // jsx構文を変数に記述できる
-  const dom =<h1>{text}</h1>;
-  return dom;
+  // reactでhtmlに属性名を記述するにはアッパーケースで記述する
+  // class名のような予約語は、classNameのようにアッパーケースで記述する
+  const dom =<h1 className="heading">{text}</h1>;
+
+  // clickイベントも同様にアッパーケースで記述する
+  // 出力されるhtmlには記述されないがイベントは問題なく実行される
+  // また、閉じタグのないhtmlは末尾に「/」を入れ、「<... />」の形で記述する
+  const input = <input type="text" onClick={() => {console.log(true)}} />
+
+  // labelのforも同様
+  const label = <label htmlFor="reactlabel">this is label</label>
+
+  return (
+    // jsxでreturnする場合、親は一つのタグでないといけない
+    <div>
+      {dom}
+      {input}
+      {label}
+    </div>
+  );
 }
 
 export default App;
