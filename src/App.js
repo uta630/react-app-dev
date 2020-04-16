2ef3a21d0086da677d701afd1c457e1a137c28f7
臼井健志 <ta-usui@media-plex.net>
2020-04-16 13:11:48 +0900

    jsxを素のreactで書くとこうなる

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 
-function App() {
-  return (
-    <div><h1>Hello world</h1></div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <p>Hello world</p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+// function App() {
+//   return (
+//     <div><h1>Hello world</h1></div>
+//   );
+// }
+class App extends Component {
+  render() {
+    return React.createElement(
+      "div",
+      null,
+      "Hello, world!"
+    )
+  }
 }
 
 export default App;
