2290d07196b99a35d1686feaedc95d93e881b925
臼井健志 <ta-usui@media-plex.net>
2020-04-17 17:45:16 +0900

    reducerの実装

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,3 @@
-// action「活動」:アプリケーション内のデータで何が起きたかを示すデータのこと
-// actionとはjavascriptのオブジェクトのこと
-// typeとtypeの値を設定し、値はユニークでないといけない
-// カウンターには「+」「-」のアクションがある
-
-// actionを定義し、返す関数をactionクリエイターと呼ぶ
-// exportすることでViewで利用できる
-
 export const INCREMENT = 'INCREMENT'
 export const DECREMENT = 'DECREMENT'
 
@@ -13,21 +5,9 @@ export const DECREMENT = 'DECREMENT'
 export const increment = () => ({
     type: INCREMENT
 })
-// 上記と同様
-// export const increment = () => {
-//     return {
-//         type: 'INCREMENT'
-//     }
-// }
         
 // actionクリエイター：decrement
 export const decrement = () => ({
     type: DECREMENT
 })
-// 上記と同様
-// export const increment = () => {
-//     return {
-//         type: 'DECREMENT'
-//     }
-// }
 
---
+++ src/reducers/count.js
@@ -0,0 +1,21 @@
+// actionのtypeをimport
+import { INCREMENT, DECREMENT } from '../actions'
+
+// stateの初期値を定義
+// オブジェクトで指定
+const initialState = { value: 0 }
+
+// reducerのindex.jsに渡すようexportする
+// reducerは関数で返し、引数を２つ持つ（state/action）
+// stateに初期値を設定
+// 関数の中ではactionの値に合わせて処理を記述する
+export default (state = initialState, action) => {
+    switch(action.type) {
+        case 'INCREMENT':
+            return { value: state.valur + 1 }
+        case 'DECREMENT':
+            return { value: state.valur + 1 }
+        default:
+            return state
+    }
+}
