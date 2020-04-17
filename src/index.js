5833f0b8d992eeec4062cb19eb2cf59a6c21ccbd
臼井健志 <ta-usui@media-plex.net>
2020-04-17 18:50:34 +0900

    storeの実装

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { createStore } from 'redux'; // storeを作成する関数
+import { Provider } from 'react-redux'; // 作成したstoreを全コンポーネントに渡すためのコンポーネント
+import reducer from './reducers'; // 作成したreducer
+import App from './components/App'; // コンポーネント用のディレクトリを作成し、Appを移動して使用
+
+// storeの作成
+// ここで作成されるstoreはアプリケーション内で唯一
+// アプリケーションのstateはこのstoreに集約されている
+const store = createStore(reducer);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // Providerで全体を囲み、store属性に上記のstoreを渡すことで利用できる
+  // これで全階層のコンポーネントでstoreが利用できるようになる
+  // propsを親から孫まで引き継がなくていい
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
