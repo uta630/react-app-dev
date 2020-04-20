cd7b433e7e5669904cc56c1a8a875ff2cd758c72
臼井健志 <ta-usui@media-plex.net>
2020-04-20 14:53:05 +0900

    connectでstate/actionの関連付け

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,47 @@
 import React, { Component } from 'react';
+// reacr-reduxのconnect関数を使って、state/actionとコンポーネントの関連付けを行う
+import { connect } from 'react-redux';
 
-// propsは変更できない値（immutable）
-// steteは変更できる値（mutable）
-// propsは親から子のコンポーネントに渡される
-// stateはコンポーネントの内部でのみ使用される
+// actionクリエイターをimport
+import { increment, decrement } from '../actions'
 
-// stateはclassコンポーネントで使用できる
-// [this.state = {...}}] として直接変更することはできない
-// 変更時にはsetState({})で対象のstateを変更する
-// stateを変更するとrenderが実行される（ここでは+/-のクリックイベントで発生）
-// この時、変更された値（count）だけ書き換えられる（jsx）
-
-const App = () => (<Counter></Counter>);
-
-class Counter extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { count: 0 }
-  }
-
-  handlePlusBtn = () => {
-    this.setState({count: this.state.count+1})
-  }
-
-  handleMinusBtn = () => {
-    this.setState({count: this.state.count-1})
-  }
+// viewのイベントで状態を遷移させて遷移後の状態を画面に描画する
+class App extends Component {
+  // comstructorで状態の初期化が必要ないので削除
+  
+  // コンポーネントで設定していた関数はactionクリエイターで実現しているので不要
 
   render() {
+    // propsに状態が入っているので宣言して使用する
+    const props = this.props
     return (
       <React.Fragment>
-        <div>count: { this.state.count }</div>
-        <button onClick={this.handlePlusBtn}>+1</button>
-        <button onClick={this.handleMinusBtn}>-1</button>
+        <div>value: { props.value }</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
       </React.Fragment>
     );
   }
 }
 
-export default App;
+//
+// stateとコンポーネントをconnectで関連付ける
+//
+
+// 「mapStateToProps」
+// stateの情報からコンポーネントに必要なものを取り出してコンポーネント内のpropsとしてマッピングする機能を持つ関数
+// 引数には状態のトップレベルを示すstateを記述
+// どういった状態をpropsとして対応させるかを関数の戻りとして記述する
+const mapStateToProps = state => ({ value: state.count.value })
+
+// 「mapDispatchToProps」
+// あるアクションが発生した時に、reducerにtypeに応じた状態変化を実行させる関数が[dispatch]という
+// このdispatch関数を引数にして、このコンポーネントに必要になるdispatch関数を宣言する
+// const mapDispatchToProps = dispatch => ({
+//   increment: () => dispatch(increment()),
+//   decrement: () => dispatch(decrement())
+// })
+const mapDispatchToProps = ({ increment, decrement }) // dispatchのショートハンド（上記と同様の動作）
+
+// 関連付けてexport
+export default connect(mapStateToProps, mapDispatchToProps)(App)
