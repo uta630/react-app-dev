314c03bf8ccafc7df57f0dad4f6e23b409eb4cde
臼井健志 <ta-usui@media-plex.net>
2020-05-06 18:10:23 +0900

    count処理を削除

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,3 @@
-// 演算
-export const INCREMENT = 'INCREMENT'
-export const DECREMENT = 'DECREMENT'
-
-export const increment = () => ({
-    type: INCREMENT
-})
-        
-export const decrement = () => ({
-    type: DECREMENT
-})
-
 // 通信
 export const READ_EVENTS = 'READ_EVENTS'
 
