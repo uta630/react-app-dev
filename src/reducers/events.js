19f97b29e7f32e78457469b2823a6b1d00d8535e
臼井健志 <ta-usui@media-plex.net>
2020-05-06 19:02:44 +0900

    データ一覧をコンポーネントで取得して表示

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,10 +1,13 @@
+import _ from 'lodash' // utility package
 import { READ_EVENTS } from '../actions'
 
-export default (state = {}, action) => {
+export default (events = {}, action) => {
     switch(action.type) {
         case READ_EVENTS:
-            return state
+            // lodashのmapKeysを用いて、
+            // オブジェクト(action.response.data)のkeyを任意の値(id)から取得して変換する
+            return _.mapKeys(action.response.data, 'id')
         default:
-            return state
+            return events
     }
 }
