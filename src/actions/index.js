81a2e0ed1b9dc82a9df4da7a55efd182b8daefe4
uta630 <ta-usui@media-plex.net>
2020-05-11 18:29:18 +0900

    データ詳細ページ作成

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios'; // 外部のhttpサーバーにリクエストを投�
 
 export const READ_EVENTS = 'READ_EVENTS'
 export const CREATE_EVENTS = 'CREATE_EVENTS'
+export const DELETE_EVENTS = 'DELETE_EVENTS'
 
 const ROOT_URL    = 'https://udemy-utils.herokuapp.com/api/v1'
 const QUERYSTRING = '?token=token123'
@@ -14,4 +15,9 @@ export const readEvents = () => async dispatch => {
 export const postEvents = values => async dispatch => {
     const response = await axios.post(`${ROOT_URL}/events${QUERYSTRING}`, values)
     dispatch({ type: CREATE_EVENTS, response })
+}
+
+export const deleteEvent = id => async dispatch => {
+    await axios.delete(`${ROOT_URL}/events/${id}${QUERYSTRING}`)
+    dispatch({ type: DELETE_EVENTS, id })
 }
--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,5 +1,5 @@
 import _ from 'lodash' // utility package
-import { READ_EVENTS } from '../actions'
+import { READ_EVENTS, DELETE_EVENTS } from '../actions'
 
 export default (events = {}, action) => {
     switch(action.type) {
@@ -7,6 +7,9 @@ export default (events = {}, action) => {
             // lodashのmapKeysを用いて、
             // オブジェクト(action.response.data)のkeyを任意の値(id)から取得して変換する
             return _.mapKeys(action.response.data, 'id')
+        case DELETE_EVENTS:
+            delete events[action.id]
+            return { ...events }
         default:
             return events
     }
